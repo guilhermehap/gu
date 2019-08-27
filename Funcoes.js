c7291bfe1556819123f4a99fcffd8061cfc23065
GUILHERME HENRIQUE ANDRADE PIMENTEL <1090481911038@fatec.local>
2019-08-27 10:34:35 -0300

    Exercicio22

--- Funcoes.js
+++ Funcoes.js
@@ -9,3 +9,33 @@ function Exercicio7(){
     }
 
 }
+
+function Exercicio22(){
+    var idade = document.getElementById("idade").value;
+    var peso = document.getElementById("peso").value;
+    if(idade <= 20){
+         if(peso <= 60 ){
+            alert("O risco é de 9")
+        }else if(peso <= 90){
+            alert("O risco é de 8")
+        }else if(peso > 90){
+            alert("O risco é de 7")
+        }
+    }else if(idade <= 50){
+        if(peso <= 60){
+            alert("O risco é de 6")
+        }else if(peso <= 90){
+            alert("O risco é de 5")
+        }else if(peso > 90){
+            alert("O risco é de 4")
+        } 
+    }else if (idade > 50){
+        if(peso <= 60){
+            alert("O risco é de 3")
+        }else if(idade <= 90){
+            alert("O risco é de 2")
+        }else if (idade > 90){
+            alert("O risco é de 1")
+        }
+    }    
+}
