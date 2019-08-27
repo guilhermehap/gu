76959c74a304dfe39dfe14c8c2f09c844d9a526b
GUILHERME HENRIQUE ANDRADE PIMENTEL <1090481911038@fatec.local>
2019-08-27 10:58:09 -0300

    Exercicio7

--- Funcoes.js
+++ Funcoes.js
@@ -1,7 +1,7 @@
 function Exercicio7(){
-    var salario = Number(document.getElementById("Informe salario").value)
+    var salario = Number(document.getElementById("salario").value);
     if (salario < 500){
-        var novo = salario + salario * 30 / 100
+        var novo = parseInt(salario) + salario  * 30 / 100
         alert("Novo valor" + novo)
     }
     else{
